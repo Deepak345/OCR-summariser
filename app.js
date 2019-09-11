4e347c0b3eec88da32ccbabf81271f48a7775452
abinashp437 <abinashp437@gmail.com>
2019-09-11 21:00:09 +0530

    testing for tesseract

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var portNumber = 8000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,23 +20,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.listen(portNumber, function() { //Runs the server on port 8000
+    console.log('Server listening at port ' + portNumber);
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var tesseract = require('node-tesseract')
+var tesseractjs = require('node-tesseract-ocr');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'OCR-summeriser' });
 });
+
+router.post('/summarise', function(req, res, next) {
+    console.log(req.body.name);
+    console.log(req.body.filename);
+    var options = {
+        l: 'eng'
+    };
+
+    //for node-tesseract-ocr package
+    tesseractjs
+        .recognize(req.body.filename, options)
+        .then(text => {
+            console.log('Result:', text)
+        })
+        .catch(err => {
+            console.log('error:', err)
+        })
+
+    //for node-tessseract package
+    tesseract
+        .process(req.body.filename, options)
+        .then(text => {
+            console.log('Result:', text)
+        })
+        .catch(err => {
+            console.log('error:', err)
+        })
+});
+
 
 module.exports = router;
