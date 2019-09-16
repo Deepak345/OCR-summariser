a1f4a3dc8e92971f7e5b4988c7e1d0be435332ea
abinashp437 <abinashp437@gmail.com>
2019-09-16 22:25:12 +0530

    added output and download

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-// var tesseract = require('node-tesseract')
 var tesseractjs = require('node-tesseract-ocr');
 var dotenv = require('dotenv');
+var path = require('path')
 var multer = require('multer');
 var GridFsStorage = require('multer-gridfs-storage');
 var Grid = require('gridfs-stream');
@@ -15,34 +15,31 @@ var mongoURI = 'mongodb://localhost:27017/ocr_summariser';
 var fs = require('fs');
 // Create mongo connection
 const conn = mongoose.createConnection(mongoURI);
+//python-shell
+let { PythonShell } = require('python-shell')
 
-// Init gfs
-let gfs;
+conn.once('open', () => {});
 
-conn.once('open', () => {
-    // Init stream
-    // gfs = Grid(conn.db, mongoose.mongo);
-    // gfs.collection('uploads');
-    //console.log("collection created")
-});
-
-/* GET home page. */
+var title = "OCR-Summariser";
+//for output of summary
+var final = {}
+    /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'OCR-summeriser' });
+    res.render('index', { title: title });
 });
 
 //Use of async function to execute according to a promise
 router.post('/summarise', upload.single('file'), async function(req, res, next) {
     var image = new Image();
     image.image = req.file.filename;
-    await image.save(function(err) {  //await
+    await image.save(function(err) { //await
         if (err) console.log(err);
         else {
             console.log("Successfuly Saved in Database");
         }
     });
 
-//for node-tesseract-ocr package
+    //for node-tesseract-ocr package
     var config = {
         lang: "eng",
         oem: 1,
@@ -50,49 +47,14 @@ router.post('/summarise', upload.single('file'), async function(req, res, next) 
     }
     console.log(image);
     console.log(__dirname);
-    await tesseractjs //await 
-    //folder access
-    .recognize(__dirname + '/../public/uploads/file-' + req.file.originalname, config)
-    //database access
-        // .recognize(image.image, config)
+    await tesseractjs //await
+        .recognize(__dirname + '/../public/uploads/file-' + req.file.originalname, config)
         .then(text => {
-            // console.log('Result:', text)
-            
-            //Save the output to a txt file inside public/output
-            fs.writeFile(__dirname + '/../public/output/file-' + req.file.originalname + '.txt', text, function(err) {
-            if(err) {
-                return console.log(err);
-                }
-            res.send("The text of the image file is extracted and saved inside public/output/"+ req.file.originalname + '.txt');
-         }); 
+            res.render('output', { title: title, subject: "The Summary", final: text });
+
         })
         .catch(err => {
             console.log('error:', err)
         })
-    // var config = {
-    //     lang: "eng",
-    //     oem: 1,
-    //     psm: 3
-    // }
-    // Image.findOne({ image: 'file-' + req.file.originalname })
-    //     .then(response => {
-    //         console.log(response);
-    //         //for node-tesseract-ocr package
-    //         tesseractjs
-    //         //folder access
-    //             .recognize('../public/uploads/file-' + req.file.originalname, config)
-    //             //database access
-    //             // .recognize(response.image, config)
-    //             .then(text => {
-    //                 console.log('Result:', text)
-    //             })
-    //             .catch(err => {
-    //                 console.log('error:', err)
-    //             })
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //     })
 });
-
 module.exports = router;
