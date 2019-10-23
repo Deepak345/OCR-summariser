78c8e952c54ca0946d4ae38fe7558c3b116a9048
Deepak345 <senapati.dipak97@gmail.com>
2019-10-23 13:15:16 +0530

    new summarisation algorithm

--- text_summarisation.py
+++ text_summarisation.py
@@ -1,35 +1,60 @@
-from sumy.utils import get_stop_words
-from sumy.nlp.stemmers import Stemmer
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer as sumytoken
-from sumy.summarizers.lex_rank import LexRankSummarizer
-from sumy.utils import get_stop_words
-from sumy.nlp.stemmers import Stemmer
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer as sumytoken
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
 
-from sumy.nlp.stemmers import Stemmer
+from nltk.tokenize import sent_tokenize, word_tokenize 
+from nltk.corpus import stopwords
 
-from sumy.summarizers.luhn import LuhnSummarizer
+from sklearn.decomposition import TruncatedSVD
 
 import sys
+# import nltk
+# nltk.download('punkt')
 
+# text pre-processing
+def preprocessing(data):
+  text = sent_tokenize(data)
+  df = pd.DataFrame(text, columns=['sent'])
 
-LANGUAGE = "english"
-SENTENCES_COUNT = 2
-stemmer = Stemmer(LANGUAGE)
-text = sys.argv[1] 
-
-def luhn_summarizer(text, stemmer, LANGUAGE, SENTENCES_COUNT):
-    parser = PlaintextParser.from_string(text, sumytoken(LANGUAGE))
-    summarizer_luhn = LuhnSummarizer(stemmer)
-    summarizer_luhn.stop_words = get_stop_words(LANGUAGE)
-    sentences = []
-    for sentence in summarizer_luhn(parser.document, SENTENCES_COUNT):
-        a = sentence
-        sentences.append(str(a))
-    return " ".join(sentences)
+  stop = stopwords.words('english')
+  df['sent'] = df['sent'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+  return text, df
 
 
 
-print(luhn_summarizer(text, stemmer, LANGUAGE, SENTENCES_COUNT))
+# summarisation function
+def summary(text, df) :
+  vectorizer = TfidfVectorizer(max_df = 0.5, min_df = 2, smooth_idf = True)
+  vectorizer.fit(df['sent'])
+  vector = vectorizer.transform(df['sent'])
+  tfidf = vector.toarray()
+  tfidf = tfidf.T
+
+  svd_model = TruncatedSVD(n_components = -(-len(text)//2),algorithm='randomized', n_iter=10, random_state=22)
+  svd_model.fit(tfidf)
+  svc = svd_model.components_
+  svc = svc.tolist()
+
+  summary = ''
+  p_pos = 0
+  for _ in range(len(svc)):
+    m = max(svc[_])
+    # print(m)
+    pos = svc[_].index(m)
+    # print(pos)
+    # print(text[pos])
+    if pos > p_pos:
+      summary = summary + ' ' + text[pos]
+    else:
+      summary = text[pos] + ' ' + summary  
+    p_pos = pos
+  return summary
+
+
+data = sys.argv[1]
+data = data.replace('\n', ' ')
+
+text,df = preprocessing(data)
+summary = summary(text, df)
+
+print(data + '\n' + summary)
